4a60ec7bc714247a63cc33cdd23cbf0982476a63
Nicolas Lépinay <nicolas@nosoft.io>
2025-06-23 14:44:40 +0200

    Emptied repo

--- src/main/java/com/mycompany/quizzroom/Main.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.quizzroom;
-
-import com.mycompany.quizzroom.cli.QuizzRoomCLI;
-
-public class Main {
-    public static void main(String[] args) {
-        QuizzRoomCLI.main(args);
-    }
-}
--- src/main/java/com/mycompany/quizzroom/model/Question.java
+++
@@ -1,6 +0,0 @@
-package com.mycompany.quizzroom.model;
-
-// Un 'record' est une classe concise idéale pour stocker des données immuables.
-public record Question(String question, String answer) {
-
-}
